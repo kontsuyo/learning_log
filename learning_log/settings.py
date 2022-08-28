62760b6eadc8698b0f09def1cdb677056672e3b9
Tsuyoshi Kondo <a9n1105@gmail.com>
2022-08-28 21:39:04 +0900

    環境変数に基づくDEBUGの設定

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -131,3 +131,8 @@ LOGIN_URL = "users:login"
 import django_heroku
 
 django_heroku.settings(locals())
+
+if os.environ.get("DEBUG") == "TRUE":
+    DEBUG = True
+if os.environ.get("DEBUG") == "FALSE":
+    DEBUG = False
